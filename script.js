470c920406363f25ba31231525012f1844e203c7
f-l7 <ateyahali432@gmail.com>
2025-05-02 15:26:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -110,7 +110,7 @@ function promoteAdmin(index) {
     if(currentRank === "✶〢𝗙𝗼𝘂𝗻𝗱𝗲𝗿") newRank = "✶ 〢𝗖𝗼 𝗢𝘄𝗻𝗲𝗿";
     else if(currentRank === "✶ 〢𝗖𝗼 𝗢𝘄𝗻𝗲𝗿") newRank = "✶〢𝗢𝘄𝗻𝗲𝗿";
     else {
-        alert("لا يمكن ترقية مدير");
+        alert("لا يمكن ترقية اكثر من Owner");
         return;
     }
 
@@ -124,10 +124,16 @@ function demoteAdmin(index) {
     const currentRank = systemData.admins[index].rank;
     let newRank = currentRank;
     
-    if(currentRank === "مدير") newRank = "رئيس";
-    else if(currentRank === "رئيس") newRank = "مشرف";
+    if(currentRank === "✶〢𝗢𝘄𝗻𝗲𝗿") newRank = "✶ 〢𝗖𝗼 𝗢𝘄𝗻𝗲𝗿";
+    if(currentRank === "✶ 〢𝗖𝗼 𝗢𝘄𝗻𝗲𝗿") newRank = "✶〢𝗙𝗼𝘂𝗻𝗱𝗲𝗿";
+    if(currentRank === "✶〢𝗙𝗼𝘂𝗻𝗱𝗲𝗿") newRank = "✶〢𝗖𝗼 𝗙𝗼𝘂𝗻𝗱𝗲𝗿";
+    if(currentRank === "✶〢𝗖𝗼 𝗙𝗼𝘂𝗻𝗱𝗲𝗿") newRank = "✶〢𝗚𝗲𝗻𝗲𝗿𝗮𝗹 𝗠𝗮𝗻𝗮𝗴𝗲𝗿";
+    if(currentRank === "✶〢𝗚𝗲𝗻𝗲𝗿𝗮𝗹 𝗠𝗮𝗻𝗮𝗴𝗲𝗿") newRank = "✶〢 𝗔𝗱𝗺𝗶𝗻 𝗠𝗮𝗻𝗮𝗴𝗲𝗿";
+    if(currentRank === "✶〢 𝗔𝗱𝗺𝗶𝗻 𝗠𝗮𝗻𝗮𝗴𝗲𝗿") newRank = "✶〢 𝗦𝘂𝗽𝗲𝗿𝘃𝗶𝘀𝗼𝗿";
+    if(currentRank === "✶〢 𝗦𝘂𝗽𝗲𝗿𝘃𝗶𝘀𝗼𝗿") newRank = "✶〢 𝗔𝗱𝗺𝗶𝗻";
+    else if(currentRank === "✶〢 𝗔𝗱𝗺𝗶𝗻") newRank = "✶〢𝗠𝗼𝗱";
     else {
-        alert("لا يمكن تنزيل مشرف");
+        alert("لا يمكن تنزيل اقل من Mod");
         return;
     }
 
